6db7f2ddca6263518d5fc816fec5c4db01711a06
kitsuyui <kitsuyui@kitsuyui.com>
2023-04-30 00:06:45 +0900

    Create Clock (meaningless)

---
+++ clock/src/Clock.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from './logo.svg'
+import './Clock.css'
+
+function Clock() {
+  return <div className="Something">2023-04-30 00:00:00</div>
+}
+
+export default Clock

e6d36e6ccea36283be604e9a51863f351a66cb94
kitsuyui <kitsuyui@kitsuyui.com>
2023-05-04 05:03:55 +0900

    Use react-use for implementing clock

--- clock/src/index.tsx
+++ clock/src/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
+import { useInterval } from 'react-use'
+
 
 export default function Clock() {
-  return <div>2023-04-30 00:00:00</div>
+  const [date, setDate] = useState(new Date())
+
+  // use useInterval instead of setInterval
+  // https://github.com/streamich/react-use/blob/master/docs/useInterval.md
+  useInterval(() => {
+    setDate(new Date())
+  }, 1000)
+
+  return <div>{date.toLocaleTimeString()}</div>
 }

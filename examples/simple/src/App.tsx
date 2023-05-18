ee6479c05993c3d4f4f6f271ede0afc05d85ac5e
kitsuyui <kitsuyui@kitsuyui.com>
2023-05-18 23:56:15 +0900

    Sound Effect when timer has completed

--- examples/simple/src/App.tsx
+++ examples/simple/src/App.tsx
@@ -17,7 +17,9 @@ import {
   TimerProps,
 } from '@kitsuyui/react-timer'
 import React from 'react'
+import useSound from 'use-sound'
 
+import Sound from './sound_effects/24.wav'
 import './App.css'
 
 const Clock = () => {
@@ -38,12 +40,22 @@ const Clock = () => {
 }
 
 const Timer = () => {
+  const [completeSound] = useSound(Sound, { playbackRate: 1 })
   return (
     <TimerContainer
-      onStart={(e) => console.log('start', e)}
-      onStop={(e) => console.log('stop', e)}
-      onComplete={(e) => console.log('complete', e)}
-      onReset={(e) => console.log('reset', e)}
+      onStart={(e) => {
+        console.log('start', e)
+      }}
+      onStop={(e) => {
+        console.log('stop', e)
+      }}
+      onComplete={(e) => {
+        console.log('complete', e)
+        completeSound()
+      }}
+      onReset={(e) => {
+        console.log('reset', e)
+      }}
     >
       <TimerContext.Consumer>
         {(timer: TimerProps) => <MinimalTimer {...timer} />}
@@ -51,13 +63,18 @@ const Timer = () => {
     </TimerContainer>
   )
 }
-
 const Stopwatch = () => {
   return (
     <StopwatchContainer
-      onStart={(e) => console.log('start', e)}
-      onStop={(e) => console.log('stop', e)}
-      onReset={(e) => console.log('reset', e)}
+      onStart={(e) => {
+        console.log('start', e)
+      }}
+      onStop={(e) => {
+        console.log('stop', e)
+      }}
+      onReset={(e) => {
+        console.log('reset', e)
+      }}
     >
       <StopwatchContext.Consumer>
         {(props: StopwatchProps) => <MinimalStopwatch {...props} />}

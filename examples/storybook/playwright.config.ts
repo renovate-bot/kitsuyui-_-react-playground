e2b7ea109a3f69b4fc612f45411887b6d1397a47
kitsuyui <kitsuyui@kitsuyui.com>
2023-05-19 23:55:41 +0900

    fix port

--- examples/storybook/playwright.config.ts
+++ examples/storybook/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
 
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:6006',
 
     // Collect trace when retrying the failed test.
     trace: 'on-first-retry',
@@ -37,7 +37,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests.
   webServer: {
     command: 'yarn start',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://127.0.0.1:6006',
     reuseExistingServer: !process.env.CI,
   },
 })

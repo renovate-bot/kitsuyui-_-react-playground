65103084bfb2a3efbdbe5390d499b34de9d1511c
kitsuyui <kitsuyui@kitsuyui.com>
2023-05-20 13:19:18 +0900

    Take a screenshot only inside the iframe

--- examples/storybook/tests/example.spec.ts
+++ examples/storybook/tests/example.spec.ts
@@ -3,5 +3,8 @@ import { test, expect } from '@playwright/test'
 test('clock page', async ({ page }) => {
   await page.goto('http://localhost:6006/?path=/story/clock--default')
   await page.waitForSelector('iframe[data-is-loaded="true"]')
-  expect(await page.screenshot()).toMatchSnapshot('clock-image.png')
+  const screenshot = await page
+    .locator('#storybook-preview-iframe')
+    .screenshot()
+  expect(screenshot).toMatchSnapshot('clock-image.png')
 })

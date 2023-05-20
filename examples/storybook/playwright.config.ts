2ba32147a124bfea63dc01328c5135a103a836d3
kitsuyui <kitsuyui@kitsuyui.com>
2023-05-20 12:29:38 +0900

    reduce diff pixels

--- examples/storybook/playwright.config.ts
+++ examples/storybook/playwright.config.ts
@@ -24,15 +24,15 @@ export default defineConfig({
     // Maximum time expect() should wait for the condition to be met.
     timeout: 5000,
 
-    toHaveScreenshot: {
-      // An acceptable amount of pixels that could be different, unset by default.
-      maxDiffPixels: 10,
-    },
+    // toHaveScreenshot: {
+    //   // An acceptable amount of pixels that could be different, unset by default.
+    //   maxDiffPixels: 1,
+    // },
 
-    toMatchSnapshot: {
-      // An acceptable ratio of pixels that are different to the total amount of pixels, between 0 and 1.
-      maxDiffPixelRatio: 0.1,
-    },
+    // toMatchSnapshot: {
+    //   // An acceptable ratio of pixels that are different to the total amount of pixels, between 0 and 1.
+    //   maxDiffPixelRatio: 0.0125,
+    // },
   },
 
   use: {

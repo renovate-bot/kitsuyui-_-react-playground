5b242a27b765d5778d2237770b0f36549328db2e
kitsuyui <kitsuyui@kitsuyui.com>
2023-05-20 13:06:35 +0900

    wait for storybook iframe loaded

--- examples/storybook/tests/example.spec.ts
+++ examples/storybook/tests/example.spec.ts
@@ -2,5 +2,6 @@ import { test, expect } from '@playwright/test'
 
 test('clock page', async ({ page }) => {
   await page.goto('http://localhost:6006/?path=/story/clock--default')
+  await page.waitForSelector('iframe[data-is-loaded="true"]')
   expect(await page.screenshot()).toMatchSnapshot('clock-image.png')
 })

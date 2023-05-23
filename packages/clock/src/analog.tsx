7560e0388e06df4e7b3018053bd08639144f5609
kitsuyui <kitsuyui@kitsuyui.com>
2023-05-24 02:07:10 +0900

    Abstraction: analog clock parameters

--- packages/clock/src/analog.tsx
+++ packages/clock/src/analog.tsx
@@ -3,6 +3,46 @@ import React from 'react'
 
 import { ClockProps } from './types'
 
+export interface AnalogClockDesign {
+  bigHandColor: string
+  smallHandColor: string
+  secondHandColor: string
+  bigHandWidth: number
+  smallHandWidth: number
+  secondHandWidth: number
+  bigHandLength: number
+  secondHandLength: number
+  minuteHandLength: number
+  width: number
+  height: number
+  frameSize: number
+  frameWidth: number
+  frameColor: string
+  frameBackgroundColor: string
+  centerPointSize: number
+  centerPointColor: string
+}
+
+export const defaultAnalogClockDesign: AnalogClockDesign = {
+  bigHandColor: 'black',
+  smallHandColor: 'black',
+  secondHandColor: 'red',
+  bigHandWidth: 2,
+  smallHandWidth: 2,
+  secondHandWidth: 2,
+  bigHandLength: 30,
+  secondHandLength: 40,
+  minuteHandLength: 40,
+  width: 100,
+  height: 100,
+  frameSize: 45,
+  frameWidth: 2,
+  frameColor: 'black',
+  frameBackgroundColor: 'white',
+  centerPointSize: 2,
+  centerPointColor: 'black',
+}
+
 export const AnalogClock: React.FunctionComponent<ClockProps> = (
   props
 ): JSX.Element => {
@@ -12,80 +52,124 @@ export const AnalogClock: React.FunctionComponent<ClockProps> = (
   const hour = dt.hour
   const minute = dt.minute
   const second = dt.second
-  const bigHandColor = 'black'
-  const smallHandColor = 'black'
-  const secondHandColor = 'red'
-  const bigHandWidth = 2
-  const smallHandWidth = 2
-  const secondHandWidth = 2
-  const bigHandLength = 30
-  const secondHandLength = 40
-  const minuteHandLength = 40
-  const centerX = 50
-  const centerY = 50
+  const {
+    bigHandColor,
+    smallHandColor,
+    secondHandColor,
+    bigHandWidth,
+    smallHandWidth,
+    secondHandWidth,
+    bigHandLength,
+    secondHandLength,
+    minuteHandLength,
+    width,
+    height,
+    frameSize,
+    frameWidth,
+    frameColor,
+    centerPointSize,
+    centerPointColor,
+    frameBackgroundColor,
+  } = defaultAnalogClockDesign
+
+  const centerX = width / 2
+  const centerY = height / 2
+
   return (
     <div
       style={{
-        width: '100px',
-        height: '100px',
+        width: `${width}px`,
+        height: `${height}px`,
       }}
     >
       <svg
-        viewBox="0 0 100 100"
+        viewBox={`0 0 ${width} ${height}`}
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        width="100"
-        height="100"
+        width={width}
+        height={height}
       >
         <circle
           cx={centerX}
           cy={centerY}
-          r="45"
-          stroke="black"
-          strokeWidth="2"
-          fill="white"
+          r={frameSize}
+          stroke={frameColor}
+          strokeWidth={frameWidth}
+          fill={frameBackgroundColor}
         />
         <circle
           cx={centerX}
           cy={centerY}
-          r="2"
-          stroke="black"
-          strokeWidth="2"
-          fill="black"
+          r={centerPointSize}
+          stroke={centerPointColor}
+          strokeWidth={centerPointSize}
+          fill={centerPointColor}
         />
         <line
           x1={centerX}
           y1={centerY}
-          x2={centerX + bigHandLength * Math.sin(hour * 30 * (Math.PI / 180))}
-          y2={centerY - bigHandLength * Math.cos(hour * 30 * (Math.PI / 180))}
+          x2={centerX + bigHandLength * hourToSin(hour)}
+          y2={centerY - bigHandLength * hourToCos(hour)}
           stroke={bigHandColor}
           strokeWidth={bigHandWidth}
         />
         <line
           x1={centerX}
           y1={centerY}
-          x2={
-            centerX + minuteHandLength * Math.sin(minute * 6 * (Math.PI / 180))
-          }
-          y2={
-            centerY - minuteHandLength * Math.cos(minute * 6 * (Math.PI / 180))
-          }
+          x2={centerX + minuteHandLength * minuteToSin(minute)}
+          y2={centerY - minuteHandLength * minuteToCos(minute)}
           stroke={smallHandColor}
           strokeWidth={smallHandWidth}
         />
         <line
           x1={centerX}
           y1={centerY}
-          x2={
-            centerX + secondHandLength * Math.sin(second * 6 * (Math.PI / 180))
-          }
-          y2={
-            centerY - secondHandLength * Math.cos(second * 6 * (Math.PI / 180))
-          }
+          x2={centerX + secondHandLength * secondToSin(second)}
+          y2={centerY - secondHandLength * secondToCos(second)}
           stroke={secondHandColor}
           strokeWidth={secondHandWidth}
         />
       </svg>
     </div>
   )
+}
+
+function hourToDegree(hour: number) {
+  return hour * 30
+}
+
+function secondToDegree(second: number) {
+  return second * 6
+}
+
+function minuteToDegree(minute: number) {
+  return minute * 6
+}
+
+function degreeToRadian(degree: number) {
+  return degree * (Math.PI / 180)
+}
+
+function hourToSin(hour: number) {
+  return Math.sin(degreeToRadian(hourToDegree(hour)))
+}
+
+function hourToCos(hour: number) {
+  return Math.cos(degreeToRadian(hourToDegree(hour)))
+}
+
+function minuteToSin(minute: number) {
+  return Math.sin(degreeToRadian(minuteToDegree(minute)))
+}
+
+function minuteToCos(minute: number) {
+  return Math.cos(degreeToRadian(minuteToDegree(minute)))
+}
+
+function secondToSin(second: number) {
+  return Math.sin(degreeToRadian(secondToDegree(second)))
+}
+
+function secondToCos(second: number) {
+  return Math.cos(degreeToRadian(secondToDegree(second)))
 }
